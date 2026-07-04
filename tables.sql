436f3804c611fcbcdb4a1e6ef081254d02ab344a
Taha <taha.visit@gmail.com>
2026-07-04 19:51:21 +0330

    refactor: change user role from varchar to type-enum

--- tables.sql
+++ tables.sql
@@ -8,11 +8,17 @@ CREATE TABLE sport (
     sport_name varchar(50) NOT NULL
 );
 
+CREATE TYPE user_role AS ENUM (
+    'USER',
+    'ADMIN',
+    'SUPPORT'
+);
+
 CREATE TABLE users (
     user_id BIGSERIAL PRIMARY KEY,
     first_name varchar(50) NOT NULL,
     last_name varchar(50) NOT NULL,
-    role varchar(50) NOT NULL,
+    role user_role NOT NULL DEFAULT 'USER',
     email varchar(255) NOT NULL UNIQUE,
     email_verified bool DEFAULT false NOT NULL,
     phone_number varchar(20) NOT NULL UNIQUE,
